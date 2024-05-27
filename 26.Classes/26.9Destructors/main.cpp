3d615fd5af17a1497b957ad3894241cc1deb422b
holongnhat <124903154+holongnhat@users.noreply.github.com>
2024-05-27 16:54:17 +0700

    Update main.cpp

--- 26.Classes/26.9Destructors/main.cpp
+++ 26.Classes/26.9Destructors/main.cpp
@@ -12,22 +12,23 @@ class Dog{
         std::string breed;
         int * p_age{nullptr};
 };
+
 Dog::Dog(std::string_view name_param, std::string_view breed_param, int  age_param){
     name = name_param;
     breed = breed_param;
-    p_age = new int;
+    p_age = new int; // giá trị của biến p_age được lưu vào vùng nhớ heap
     *p_age = age_param;
-    std::cout << "Dog constructor called for " << name << std::endl;
+    std::cout << "Dog CONSTRUCTOR called for " << name << std::endl;
 }
 
-Dog::~Dog(){
+Dog::~Dog(){ 
     delete p_age;
-    std::cout << "Dog destructor called for : " << name << std::endl;
+    std::cout << "Dog DESTRUCTOR called for : " << name << std::endl;
 }
 
 void some_func(){
-    Dog* p_dog = new Dog("Fluffy","Shepherd",2);
-
+    Dog* p_dog = new Dog("Fluffy","Shepherd",2); 
+    // ("Fluffy", "Shepherd", 2) được lưu vào vùng nhớ heap.
     delete p_dog;// Causes for the destructor of Dog to be called
 }
 
