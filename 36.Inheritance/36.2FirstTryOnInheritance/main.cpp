4febca5f0a3759471ccaa899e5a124b907b44548
holongnhat <124903154+holongnhat@users.noreply.github.com>
2024-05-27 21:37:27 +0700

    Update main.cpp

--- 36.Inheritance/36.2FirstTryOnInheritance/main.cpp
+++ 36.Inheritance/36.2FirstTryOnInheritance/main.cpp
@@ -4,9 +4,9 @@
 
 int main(){
 
-    Player p1("Basketball");
-    p1.set_first_name("John");
-    p1.set_last_name("Snow");
+    Player p1("Basketball"); // --> player.h --> game_param = Basketball --> player.cpp --> m_game = game_param -> player.m_game
+    p1.set_first_name("John"); // Set được do Player được thừa hưởng từ Person !
+    p1.set_last_name("Snow"); 
     std::cout << "player : " << p1 << std::endl;
    
     return 0;
